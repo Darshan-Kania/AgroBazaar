e836ce9ec3a8d4cf261fe9804286158a469fd6a5
Darshan-Kania <darshankania.ddu0109@gmail.com>
2025-09-21 12:09:01 +0530

    Add initial project files, including .gitignore, solution, project, and configuration files

---
+++ Program.cs
@@ -0,0 +1,24 @@
+namespace AgroBazaar
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                    config.AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true);
+                    config.AddJsonFile("appsettings.secrets.json", optional: true, reloadOnChange: true);
+                    config.AddEnvironmentVariables();
+                });
+    }
+}
